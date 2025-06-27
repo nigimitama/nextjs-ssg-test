ac29a9393ba430bd7474aad662b41270cd251502
nigimitama <nigimitama16@gmail.com>
2025-06-28 08:45:44 +0900

    replace a with Link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function Page() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -6,9 +8,9 @@ export default function Page() {
         <p className="text-lg">Here is my portfolio</p>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="/">
+        <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="/">
           Home
-        </a>
+        </Link>
       </footer>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -35,15 +37,10 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/about"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="/about">
           <img aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
           about
-        </a>
+        </Link>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
